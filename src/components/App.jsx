008de9b6d0a6e557ca0e01e7e25355e114f62423
Anastasiia Andrusenko <107683074+Anastasiia-Andrusenko@users.noreply.github.com>
2024-05-06 12:08:19 +0300

    normolized

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,8 @@
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>Привіт, я Настя!</h1>
+      <p>можу спілкуватись EN UA </p>
     </div>
   );
 };
