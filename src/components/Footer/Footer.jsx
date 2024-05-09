ca34a45d7db679b472aaec6269ad58e56516410b
Anastasiia Andrusenko <107683074+Anastasiia-Andrusenko@users.noreply.github.com>
2024-05-09 17:11:41 +0300

    responsive home page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,37 +5,39 @@ import langArr from '../../lang';
 const Footer = ({ currentLanguage }) => {
   return (
     <footer className={css.footer}>
-      <p className={css.callToAction}>
-        {currentLanguage === 'ua' ? langArr.footer.ua : langArr.footer.en}
-        <span className={css.hand}>
-          <FaRegHandPointRight />
-        </span>
-      </p>
-      <ul className={css.contactsList}>
-        <li className={css.contactItem}>
-          <a href="mailto:nastya.andrus@gmail.com" target="blank">
-            nastya.andrus@gmail.com
-          </a>
-        </li>
-        <li className={css.contactItem}>
-          <a href="tel:+380996434033" target="blank">
-            +380 99 64 34 033
-          </a>
-        </li>
-        <li className={css.contactItem}>
-          <a
-            href="https://www.facebook.com/anastasiia.andrusenko"
-            target="blank"
-          >
-            facebook
-          </a>
-        </li>
-        <li className={css.contactItem}>
-          <a href="https://join.skype.com/invite/cFrUyX19Qw90" target="blank">
-            skype
-          </a>
-        </li>
-      </ul>
+      <div className={css.container}>
+        <p className={css.callToAction}>
+          {currentLanguage === 'ua' ? langArr.footer.ua : langArr.footer.en}
+          <span className={css.hand}>
+            <FaRegHandPointRight />
+          </span>
+        </p>
+        <ul className={css.contactsList}>
+          <li className={css.contactItem}>
+            <a href="mailto:nastya.andrus@gmail.com" target="blank">
+              nastya.andrus@gmail.com
+            </a>
+          </li>
+          <li className={css.contactItem}>
+            <a href="https://join.skype.com/invite/cFrUyX19Qw90" target="blank">
+              skype
+            </a>
+          </li>
+          <li className={css.contactItem}>
+            <a href="tel:+380996434033" target="blank">
+              +380 99 64 34 033
+            </a>
+          </li>
+          <li className={css.contactItem}>
+            <a
+              href="https://www.facebook.com/anastasiia.andrusenko"
+              target="blank"
+            >
+              facebook
+            </a>
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -11,18 +11,22 @@ const Home = ({ currentLanguage }) => {
   return (
     <>
       <div className={css.container}>
-        <h1 className={css.hi}>
-          {currentLanguage === 'ua' ? langArr.welcome.ua : langArr.welcome.en}
-          <br />
-          {currentLanguage === 'ua' ? langArr.welcome.ua2 : langArr.welcome.en2}
-        </h1>
-        <div className={css.portrait}></div>
         <LangToggle isHeader={false} />
-        <div className={css.skills}>
-          <h2 className={css.title}>
-            {currentLanguage === 'ua' ? langArr.like.ua : langArr.like.en}
-          </h2>
-          <Navigation currentLanguage={currentLanguage} />
+        <div className={css.hero}>
+          <h1 className={css.hi}>
+            {currentLanguage === 'ua' ? langArr.welcome.ua : langArr.welcome.en}
+            <br />
+            {currentLanguage === 'ua'
+              ? langArr.welcome.ua2
+              : langArr.welcome.en2}
+          </h1>
+          <div className={css.portrait}></div>
+          <div className={css.skills}>
+            <h2 className={css.title}>
+              {currentLanguage === 'ua' ? langArr.like.ua : langArr.like.en}
+            </h2>
+            <Navigation currentLanguage={currentLanguage} />
+          </div>
         </div>
       </div>
       <Footer currentLanguage={currentLanguage} />
